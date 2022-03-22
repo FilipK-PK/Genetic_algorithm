693108bf86c319b467460a147319687bcfa5d634
Filip Kostkiewicz <ftsk.hj@wp.pl>
2022-03-22 10:53:00 +0100

    Aktualizacja funkcji testujacej parametry

--- src/test/test.py
+++ src/test/test.py
@@ -67,7 +67,7 @@ class Test:
         self.__invert = None
 
     """ Glowna funkcja, ona steruje wsztstkim """
-    def run(self) -> None:
+    def run(self) -> float:
         self.__init_extr_class()  # inicjacja zewnetrznych klass
         popu = self.__create_population()  # tworzenie populacji
 
@@ -81,7 +81,7 @@ class Test:
             popu = self.__mutation.mutate(popu)  # mutacja populacji
             popu = self.__invert.invert(popu)  # inwersja populacji
 
-        self.__show_result()
+        return self.__show_result()
 
     """ Funkcja generuje polulację """
     def __create_population(self) -> []:
@@ -111,15 +111,36 @@ class Test:
     """ Pokazuje min globalne """
     def __show_result(self) -> None:
         best_min, best_yx = self.__statistic.get_global_best()
-        print(best_min, end=SEP)
+        return best_min
+
+
+def calc_statistic():
+    """ Sprawdzanie 40 razy kazdej wartosci danego parametru """
+
+    """ Przykładaowe wywołanie """
+    data = []
+    var = [4, 8, 12, 16, 20, 24, 28, 32]
+
+    """ Petla oblicza poszczegulne wartosci """
+    for el in var:
+        row = []
+        print('[S]:', el)
+        for _ in range(40):
+            app = Test(len_bit=el)
+            row.append(app.run())
+        data.append(row)
+
+    data = np.array(data)
+
+    """ Obliczanie min sredniej odchylenia """
+    min_val = np.min(data, axis=1)
+    mean_val = np.mean(data, axis=1)
+    std_val = np.std(data, axis=1)
+
+    """ Wyswietlanie wyników """
+    for i, j, k, l in zip(var, min_val, mean_val, std_val):
+        print(i, SEP, j, SEP, k, SEP, l)
 
 
 if __name__ == '__main__':
-    """ Sprawdzanie 10 razy kazdej wartosci danego parametru """
-
-    """ Przykładowe wywołanie """
-    for el in ['Koła ruletki', 'Najlepsi', 'Selekcja turniejowa']:
-        for _ in range(10):
-            app = Test(name_select=el)
-            app.run()
-        print()
+    calc_statistic()
